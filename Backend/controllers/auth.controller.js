de75a44edb5fa6119a947ae46ff4f7cc45ac5755
KihyeokK <joe.brighter20@gmail.com>
2023-11-18 15:06:53 -0500

    add authentication

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -32,7 +32,32 @@ exports.register = async (req, res) => {
 
         const createdUser = await newUser.save();
         res.status(201).json(createdUser);
-    } catch (err) {
-        res.status(500).json({ error: err });
+    } catch (error) {
+        console.log("user registration failed");
+        res.status(500).json({ error: error });
     }
 }   
+
+exports.login = async (req, res) => {
+    try {
+        const { userName, password } = req.body;
+        const foundUser = await User.findOne({ userName: userName });
+        if (!foundUser) return res.status(400).json({ message: "This user does not exist." });
+    
+        const isPWDValid = await bcrypt.compare(password, user.password);
+        if (!isPWDValid) return res.status(400).json({ message: "Invalid username or password." });
+ 
+        res.status(200).json(foundUser);
+      } catch (error) {
+        console.log("login failed");
+        res.status(500).json({ error: error });
+      }
+}
+
+exports.logout = (req, res, next) => {
+    req.session.destroy((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Logout failed."});
+    });
+    res.status(200).json({ message: "Successfully Logged out!"});
+  };
--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,11 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 require("dotenv").config();
+const csrf = require("csurf");
+
+// Store sesion
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 // import routes here
 
@@ -13,6 +18,40 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const sessionStore = new MongoDBStore({
+  uri: MONGODB_URI,
+  collections: "sessions",
+});
+const csrfProtection = csrf(); // middlware for csrf protection
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
+app.use(csrfProtection);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    // if user is not logged in
+    console.log("The user is not present in the session.");
+    next();
+  } else {
+    console.log("getting user");
+    // if user is logged in, get user info from database
+    // user is fetched in every request while the session is not destroyed(while user is logged in)
+    User.findById(req.session.user._id)
+      .then((user) => {
+        // all good if user is found in session
+        next();
+      })
+      .catch((error) => res.status(500).json({ message: "user not found in session" }));
+  }
+});
 
 app.get('/', (req, res) => {
     res.send("Hello World");
